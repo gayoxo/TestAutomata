5f982ef91cc3b119df2fb67de301ceffe6dcb636
Joaquin Gayoso Cabada <jgayoso@ucm.es>
2017-06-14 12:13:44 +0200

    Ala, vamos a ello poco a poco

---
+++ TestAutomataDFA_NFA/src/fdi/ucm/es/dfa/DFAManager.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package fdi.ucm.es.dfa;
+
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+
+import fdi.ucm.es.model.DocumentsV;
+
+/**
+ * @author Joaquin Gayoso Cabada
+ *
+ */
+public class DFAManager {
+
+	public DFAManager(List<DocumentsV> documentos) {
+
+Stack<StateDFA> PilaProcesar = new Stack<StateDFA>();
+Queue<StateDFA> Viables =new PriorityQueue<StateDFA>();
+	}
+
+}
---
+++ TestAutomataDFA_NFA/src/fdi/ucm/es/model/DocumentsV.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package fdi.ucm.es.model;
+
+import java.util.ArrayList;
+
+/**
+ * @author Joaquin Gayoso Cabada
+ *
+ */
+public class DocumentsV {
+	
+	private ArrayList<Long> Att;
+	private Long Id;
+
+	@SuppressWarnings("unused")
+	private DocumentsV() {
+		Att=new ArrayList<Long>();
+		this.Id=0l;
+	}
+	
+	public DocumentsV(Long Id) {
+		Att=new ArrayList<Long>();
+		this.Id=Id;
+		
+	}
+	
+	public ArrayList<Long> getAtt() {
+		return Att;
+	}
+
+	public void setAtt(ArrayList<Long> att) {
+		Att = att;
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+	
+	
+	
+	
+
+}
