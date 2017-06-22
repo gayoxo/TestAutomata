68fa65d1fe90a281523b76ef48d9e0c039a683a4
Joaquin Gayoso Cabada <jgayoso@ucm.es>
2017-06-22 19:46:56 +0200

    a CAsa

--- TestAutomataDFA_NFA/src/fdi/ucm/es/Principal.java
+++ TestAutomataDFA_NFA/src/fdi/ucm/es/Principal.java
@@ -34,7 +34,7 @@ public class Principal {
 	
 	private static final Float _DOWN_LIMIT = 0.1f; //1+_DOWN_LIMIT=f(1)
 	public static boolean Debug=true;
-	public static boolean Ayuda=false;
+//	public static boolean Ayuda=false;
 
 	/**
 	 * @param args
