70504f118cf7e229bbb9e8333d74d1e6e4cd83ab
Joaquin Gayoso Cabada <jgayoso@ucm.es>
2017-06-22 19:03:54 +0200

    puesto el limite de navegaciones mas alto

--- TestAutomataDFA_NFA/src/fdi/ucm/es/Principal.java
+++ TestAutomataDFA_NFA/src/fdi/ucm/es/Principal.java
@@ -32,7 +32,7 @@ import fdi.ucm.es.nfa.NFAManager;
 public class Principal {
 	
 	
-	private static final Float _DOWN_LIMIT = 0.01f; //1+_DOWN_LIMIT=f(1)
+	private static final Float _DOWN_LIMIT = 0.1f; //1+_DOWN_LIMIT=f(1)
 	public static boolean Debug=true;
 	public static boolean Ayuda=false;
 
